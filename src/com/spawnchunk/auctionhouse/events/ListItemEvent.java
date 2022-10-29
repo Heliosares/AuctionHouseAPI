2f4a422564ebdf63c69b36721af1e8ee1ef01e40
Heliosares <kyler.t.skeens@gmail.com>
2022-10-28 21:49:04 -0400

    Initial commit

---
+++ src/com/spawnchunk/auctionhouse/events/ListItemEvent.java
@@ -0,0 +1,27 @@
+package com.spawnchunk.auctionhouse.events;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.inventory.ItemStack;
+
+public class ListItemEvent {
+
+	public double getPrice() {
+		return 0;
+	}
+
+	public ItemStack getItem() {
+		return null;
+	}
+
+	public String getSeller_UUID() {
+		return null;
+	}
+
+	public String getWorld() {
+		return null;
+	}
+
+	public OfflinePlayer getSeller() {
+		return null;
+	}
+}
---
+++ src/com/spawnchunk/auctionhouse/events/PurchaseItemEvent.java
@@ -0,0 +1,31 @@
+package com.spawnchunk.auctionhouse.events;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.inventory.ItemStack;
+
+public class PurchaseItemEvent {
+
+	public OfflinePlayer getBuyer() {
+		return null;
+	}
+
+	public String getBuyer_UUID() {
+		return null;
+	}
+
+	public double getPrice() {
+		return 0;
+	}
+
+	public ItemStack getItem() {
+		return null;
+	}
+
+	public String getWorld() {
+		return null;
+	}
+
+	public OfflinePlayer getSeller() {
+		return null;
+	}
+}
