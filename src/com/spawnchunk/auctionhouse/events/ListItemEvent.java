756e099dff6a2ba1b261c239d82f180776e43546
Heliosares <kyler.t.skeens@gmail.com>
2022-10-28 23:09:39 -0400

    Update methods

--- src/com/spawnchunk/auctionhouse/events/ListItemEvent.java
+++ src/com/spawnchunk/auctionhouse/events/ListItemEvent.java
@@ -1,21 +1,12 @@
 package com.spawnchunk.auctionhouse.events;
 
 import org.bukkit.OfflinePlayer;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 
-public class ListItemEvent {
-
-	public double getPrice() {
-		return 0;
-	}
-
-	public ItemStack getItem() {
-		return null;
-	}
-
-	public String getSeller_UUID() {
-		return null;
-	}
+public class ListItemEvent extends Event implements Cancellable {
 
 	public String getWorld() {
 		return null;
@@ -23,5 +14,38 @@ public class ListItemEvent {
 
 	public OfflinePlayer getSeller() {
 		return null;
+	}
+
+	public String getSeller_UUID() {
+		return null;
+	}
+
+	public float getPrice() {
+		return 0;
+	}
+
+	public void setPrice(float price) {
+	}
+
+	public ItemStack getItem() {
+		return null;
+	}
+
+	public void setItem(ItemStack item) {
+	}
+
+	public HandlerList getHandlers() {
+		return null;
+	}
+
+	public static HandlerList getHandlerList() {
+		return null;
+	}
+
+	public boolean isCancelled() {
+		return false;
+	}
+
+	public void setCancelled(boolean cancelled) {
 	}
 }
--- src/com/spawnchunk/auctionhouse/events/PurchaseItemEvent.java
+++ src/com/spawnchunk/auctionhouse/events/PurchaseItemEvent.java
@@ -1,25 +1,12 @@
 package com.spawnchunk.auctionhouse.events;
 
 import org.bukkit.OfflinePlayer;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 
-public class PurchaseItemEvent {
-
-	public OfflinePlayer getBuyer() {
-		return null;
-	}
-
-	public String getBuyer_UUID() {
-		return null;
-	}
-
-	public double getPrice() {
-		return 0;
-	}
-
-	public ItemStack getItem() {
-		return null;
-	}
+public class PurchaseItemEvent extends Event implements Cancellable {
 
 	public String getWorld() {
 		return null;
@@ -27,5 +14,46 @@ public class PurchaseItemEvent {
 
 	public OfflinePlayer getSeller() {
 		return null;
+	}
+
+	public OfflinePlayer getBuyer() {
+		return null;
+	}
+
+	public String getSeller_UUID() {
+		return null;
+	}
+
+	public String getBuyer_UUID() {
+		return null;
+	}
+
+	public float getPrice() {
+		return 0;
+	}
+
+	public void setPrice(float price) {
+	}
+
+	public ItemStack getItem() {
+		return null;
+	}
+
+	public void setItem(ItemStack item) {
+	}
+
+	public HandlerList getHandlers() {
+		return null;
+	}
+
+	public static HandlerList getHandlerList() {
+		return null;
+	}
+
+	public boolean isCancelled() {
+		return false;
+	}
+
+	public void setCancelled(boolean cancelled) {
 	}
 }
